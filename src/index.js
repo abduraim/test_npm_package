e24dd3a571a695e90d7a88ad0ea89bffccafc49a
at <at@101media.ru>
2019-06-07 20:04:13 +0300

    first publish

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+import './css/style.css';
+
+
+
+// let TimSlider = function(htmlElement = undefined) {
+//
+//     this.element = htmlElement || document.getElementById('tim_slider');
+//
+//     console.log(this.element);
+//
+// };
+//
+// console.log('component mounted1');
+//
+// let timSlider = new TimSlider();
+//
+// console.log(timSlider);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    plugins: [
+        //new CleanWebpackPlugin()
+    ],
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
+};
